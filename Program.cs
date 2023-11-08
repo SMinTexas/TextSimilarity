9f6e66c443996cfd39d33d21e0bc29d638665a45
Steven Murray <steve.murray@omnicell.com>
2023-11-08 13:56:51 -0600

    Change the GPT model to 4

--- Program.cs
+++ Program.cs
@@ -130,7 +130,7 @@ class Program
     public class Request
     {
         [JsonPropertyName("model")]
-        public string Model { get; set; } = "gpt-3.5-turbo";
+        public string Model { get; set; } = "gpt-4";
         [JsonPropertyName("max_tokens")]
         public int MaxTokens { get; set; } = 4000;
         [JsonPropertyName("messages")]
